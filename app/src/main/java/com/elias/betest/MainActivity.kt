15c8e34fc45ea4c8536adeb895d1b42938662c7a
Azurian <jdiaz@azurian.com>
2020-10-19 20:36:24 -0300

    first commit

--- app/src/main/java/com/elias/betest/MainActivity.kt
+++ app/src/main/java/com/elias/betest/MainActivity.kt
@@ -2,7 +2,6 @@ package com.elias.betest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.elias.belibrary.MonitoringBeacon
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("algo" , "main open!!!")
+
         val monitor = MonitoringBeacon(this,"8cbde5be-b52d-4466-875e-bd6a7c308978",null,true)
         monitor.init()
     }
