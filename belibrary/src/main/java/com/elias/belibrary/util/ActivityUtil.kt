f55b0a99d29366bbe9332ea578b05c1457da607d
Azurian <jdiaz@azurian.com>
2020-11-15 12:41:03 -0300

    add https

--- belibrary/src/main/java/com/elias/belibrary/util/ActivityUtil.kt
+++ belibrary/src/main/java/com/elias/belibrary/util/ActivityUtil.kt
@@ -2,7 +2,7 @@ package com.elias.belibrary.util
 
 object ActivityUtil {
 
-    const val apiEncontrado = "http://beaconbat.com/bserv-2/resources/ads/consultar"
+    const val apiEncontrado = "https://beaconbat.com/bserv-2/resources/ads/consultar"
     var utilView : Util? = null
     var listaBeaconEncontrados = ArrayList<String>()
     var noIniciado : Boolean = true
